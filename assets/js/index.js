ea7ea527aaa1d0b2b7808194a5d178ed9b99caa2
alicekb <alicekb01@gmail.com>
2019-06-14 17:49:25 -0400

    Add remaining attributes to Pokemon Class

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,25 @@ document.addEventListener('DOMContentLoaded', function(event) {
   handleOnSearchSubmit();
 });
 
+// flavor_text_entries(en), genera(en)
 class Pokemon {
-  constructor(args) {
-    this.name = args.name;
-    this.stats = this.formatStatsArray(args.stats);
+  constructor(pokemonData, pokemonSpeciesData) {
+    this.name = pokemonData.name;
+    this.stats = this.formatStatsArray(pokemonData.stats);
+    this.height = pokemonData.height;
+    this.species = pokemonData.species.name;
+    this.weight = pokemonData.weight;
+    this.abilities = this.formatAbilities(pokemonData.abilities);
+    this.types = this.formatTypes(pokemonData.types);
+    this.sprites = pokemonData.sprites;
+    this.evolvesFrom = pokemonSpeciesData.evolves_from_species
+      ? pokemonSpeciesData.evolves_from_species.name
+      : '';
+    this.color = pokemonSpeciesData.color.name;
+    this.description = this.findEnglishDescription(
+      pokemonSpeciesData.flavor_text_entries
+    );
+    this.genus = pokemonSpeciesData.genera[2].genus;
   }
 
   formatStatsArray(stats) {
@@ -14,30 +29,49 @@ class Pokemon {
       baseStat: stat.base_stat,
     }));
   }
+
+  formatAbilities(abilities) {
+    return abilities.map(({ ability }) => ability.name);
+  }
+
+  formatTypes(types) {
+    return types.map(({ type }) => type.name);
+  }
+
+  findEnglishDescription(flavorText) {
+    return flavorText.filter(textObject => textObject.language.name === 'en')[0]
+      .flavor_text;
+  }
 }
 
 Pokemon.prototype.formatHtml = function() {
-  return `<div class="span-col-2">${this.name}</div>
+  return `<div class="span-col-2">${this.name.toUpperase()}</div>
           <div>picture</div>
           <div>profile</div>
           <div>type</div>
           <div>stats</div>
-          <div class="span-col-2">description</div>`;
+          <div class="span-col-2">
+            <h3>description</h3>
+            <p>${this.description}</p>
+          </div>`;
 };
 
 function handleOnSearchSubmit() {
   const submitButton = document.getElementById('search-button');
 
-  submitButton.addEventListener('click', function() {
+  submitButton.addEventListener('click', async function() {
     const inputValue = document.getElementById('search-input').value;
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}/`)
-      .then(res => res.json())
-      .then(data => {
-        const pokemon = new Pokemon(data);
-        const formatedHtml = pokemon.formatHtml();
+    const pokemonData = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${inputValue}/`
+    ).then(res => res.json());
 
-        document.getElementsByClassName('grid')[0].innerHTML = formatedHtml;
-      });
+    const pokemonSpeciesData = await fetch(
+      `https://pokeapi.co/api/v2/pokemon-species/${inputValue}/`
+    ).then(res => res.json());
+
+    const pokemon = new Pokemon(pokemonData, pokemonSpeciesData);
+    const formatedHtml = pokemon.formatHtml();
+    document.getElementsByClassName('grid')[0].innerHTML = formatedHtml;
   });
 }
