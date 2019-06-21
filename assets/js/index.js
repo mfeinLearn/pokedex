27acfbebd9128b967cdb729978d0ca19ed97820c
alicekb <alicekb01@gmail.com>
2019-06-21 12:44:10 -0400

    Update Pokemon class with static class functions.

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,12 +6,12 @@ document.addEventListener('DOMContentLoaded', function(event) {
 class Pokemon {
   constructor(pokemonData, pokemonSpeciesData) {
     this.name = pokemonData.name;
-    this.stats = this.formatStatsArray(pokemonData.stats);
+    this.stats = Pokemon.formatStatsArray(pokemonData.stats);
     this.height = pokemonData.height;
     this.species = pokemonData.species.name;
     this.weight = pokemonData.weight;
-    this.abilities = this.formatAbilities(pokemonData.abilities);
-    this.types = this.formatTypes(pokemonData.types);
+    this.abilities = Pokemon.formatAbilities(pokemonData.abilities);
+    this.types = Pokemon.formatTypes(pokemonData.types);
     this.sprites = [
       pokemonData.sprites.front_default,
       pokemonData.sprites.front_female,
@@ -20,28 +20,28 @@ class Pokemon {
       ? pokemonSpeciesData.evolves_from_species.name
       : 'n/a';
     this.color = pokemonSpeciesData.color.name;
-    this.description = this.findEnglishDescription(
+    this.description = Pokemon.findEnglishDescription(
       pokemonSpeciesData.flavor_text_entries
     );
     this.genus = pokemonSpeciesData.genera[2].genus;
   }
 
-  formatStatsArray(stats) {
+  static formatStatsArray(stats) {
     return stats.map(stat => ({
       name: stat.stat.name,
       baseStat: stat.base_stat,
     }));
   }
 
-  formatAbilities(abilities) {
+  static formatAbilities(abilities) {
     return abilities.map(({ ability }) => ability.name);
   }
 
-  formatTypes(types) {
+  static formatTypes(types) {
     return types.map(({ type }) => type.name);
   }
 
-  findEnglishDescription(flavorText) {
+  static findEnglishDescription(flavorText) {
     return flavorText.filter(textObject => textObject.language.name === 'en')[0]
       .flavor_text;
   }
